41c3f7e62174d2440fe1f7fda123891f28ec12ce
Julia dos Santos <santos.ju1995@gmail.com>
2023-01-19 23:37:49 -0300

    Exercicio for

--- exercicio_for/src/exercicio_for/Main.java
+++ exercicio_for/src/exercicio_for/Main.java
@@ -38,5 +38,30 @@ public class Main {
 
 	public static void exercicio2() {
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int in = 0;
+		int out = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			int x = scanner.nextInt();
+			if (x >= 10 && x <= 20) {
+				in = in + 1;
+				
+			}	
+			
+			else {
+				out = out + 1;
+			}
+				
+		}
+		
+		System.out.println("in " + in);
+		System.out.println("out " + out);
+		
+		scanner.close();
+		
 	}
 }
+
