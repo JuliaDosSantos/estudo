456e3393983f55444a3adaf58c81a73a29702959
Julia dos Santos <santos.ju1995@gmail.com>
2023-01-24 23:36:05 -0300

    Exercícios for

--- exercicio_for/src/exercicio_for/Main.java
+++ exercicio_for/src/exercicio_for/Main.java
@@ -1,5 +1,6 @@
 package exercicio_for;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +17,14 @@ public class Main {
 		
 		if (codigoExercicio == 2) {
 			exercicio2();
+		}
+		
+		if (codigoExercicio == 3) {
+			exercicio3();
+		}
+		
+		if (codigoExercicio == 4) {
+			exercicio4();
 		}
 	}
 	
@@ -59,6 +68,55 @@ public class Main {
 		
 		System.out.println("in " + in);
 		System.out.println("out " + out);
+		
+		scanner.close();
+		
+	}
+	
+	public static void exercicio3() {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for (int i = 1; i < N; i++ ) {
+			
+			double a = scanner.nextDouble();
+			double b = scanner.nextDouble();
+			double c = scanner.nextDouble();
+			
+			double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+			
+			System.out.printf("%.1f%n", media);
+		
+		}
+		
+		scanner.close();
+		
+	}
+	
+	public static void exercicio4() {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for (int i = 0; i < N; i++) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			
+			if (B == 0) {
+				System.out.println("Divisão impossível");
+			}
+		
+			else {
+				double divisao = (double) A / B;
+				System.out.printf("%.1f%n", divisao);
+			}
+			
+		}
 		
 		scanner.close();
 		
